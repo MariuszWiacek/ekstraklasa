ebdee0fef67e0fc951bdabd8636d29df0690da29
MariuszLillup <Mariusz@lillup.com>
2024-12-14 17:32:59 +0000

    zyczenia

--- src/pages/bets.js
+++ src/pages/bets.js
@@ -325,8 +325,8 @@ const Bets = () => {
     ))
   ) : (
     <tr>
-      <td colSpan="6" style={{ textAlign: 'center' }}>
-        Koniec rundy jesiennej.
+      <td colSpan="6" style={{ textAlign: 'center', padding:'5%' }}>
+      Koniec rundy jesiennej. <br></br><br></br> Wracamy po przerwie zimowej. Dziękujemy za zaciekłą grę do końca.<br></br> Życzymy Wesołych Świąt oraz Szczęśliwego Nowego Roku!
       </td>
     </tr>
   )}
